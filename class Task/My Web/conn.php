8322f1d123dfc76a83f89c8cb8fd1456f5e84c29
IamMuqaddasShehzadi <muqadasshehzadi818@gmail.com>
2022-08-10 21:11:34 +0500

    first commit

---
+++ class Task/My Web/conn.php
@@ -0,0 +1,6 @@
+<?php
+	$conn=mysqli_connect('localhost','root','','mydb');
+	if(!$conn){
+		echo "NO";
+	}
+?>
---
+++ class Task/My Web/index.php
@@ -0,0 +1,13 @@
+<?php
+include 'conn.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>My Web</title>
+</head>
+<body>
+
+</body>
+</html>
